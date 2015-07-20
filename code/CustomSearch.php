bcf4891ec9767b08e75281e8acfb3adb98b51408
wernerkrauss <werner.krauss@netwerkstatt.at>
2015-07-20 17:17:52 +0200

    Make number of items per page configurable
    also gets pagination working, cause $ret->setLimitItems(0) just doesn't work...

--- code/CustomSearch.php
+++ code/CustomSearch.php
@@ -8,6 +8,11 @@
  */
 class CustomSearch extends Extension {
 
+	/**
+	 * @var int the number of items for each page, used for pagination
+	 */
+	private static $items_per_page = 10;
+
 	static $allowed_actions = array(
 			'SearchForm',
 			'results',
@@ -17,7 +22,7 @@ class CustomSearch extends Extension {
 	 * Site search form
 	 */
 	public function SearchForm() {
-		
+
 		$searchText = _t('SearchForm.SEARCH', 'Search');
 
 		if ($this->owner->request && $this->owner->request->getVar('Search')) {
@@ -44,37 +49,38 @@ class CustomSearch extends Extension {
 	public function getSearchResults($request) {
 
 		$list = new ArrayList();
-						
+
 		$v = $request->getVars();
 		if (!isset($v["start"]))
 			$v["start"] = 0;
 
 		$q = $v["Search"];
 		$s = $v["start"];
-		
+
 		$input = DB::getConn()->addslashes($q);
 		$data = DB::query("SELECT * FROM SearchableDataObjects WHERE MATCH (Title, Content) AGAINST ('$input' IN NATURAL LANGUAGE MODE)");
-		
+
 		foreach ($data as $row) {
-			
+
 			$do = DataObject::get_by_id($row['ClassName'], $row['ID']);
 			$do->Title = $row['Title'];
 			$do->Content = $row['Content'];
-						
+
 			$list->push($do);
 		}
-			
+
+		$pageLength = Config::inst()->get('CustomSearch', 'items_per_page');
 		$ret = new PaginatedList($list);
-		$ret->setPageLength(10);
+		$ret->setPageLength($pageLength);
 		$ret->setPageStart($s);
 		$ret->setTotalItems($list->count());
-		$ret->setLimitItems(0);
-		
+		$ret->setLimitItems($pageLength);
+
 		return $ret;
 	}
 
 	public function results($data, $form, $request) {
-		
+
 		$data = array(
 				'Results' => $this->getSearchResults($request),
 				'Query' => $form->getSearchQuery(),
