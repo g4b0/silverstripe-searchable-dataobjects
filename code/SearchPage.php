8313b378fcd94e3db1bebc6934b2224f71ae9713
Werner Krauss <werner.krauss@netwerkstatt.at>
2015-09-20 18:19:20 +0200

    adding a simple search page type for linking to Setting CustomSearch.search_controller to "SearchPage" will search for the first SearchPage (e.g. with SEO friendly url segment) instead of the current page. Fixes #15

---
+++ code/SearchPage.php
@@ -0,0 +1,24 @@
+<?php
+
+class SearchPage extends Page {
+
+    private static $db = array(
+    );
+
+    private static $has_one = array(
+    );
+
+	private static $defaults = array(
+        'ShowInMenu' => false
+    );
+
+    public function getCMSFields() {
+        $fields = parent::getCMSFields();
+
+        return $fields;
+    }
+}
+
+class SearchPage_Controller extends Page_Controller {
+
+}
