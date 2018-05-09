88198276b4fffc8cac12f7a7d216940b5e0578e5
Anton Smith <anton@firebrand.nz>
2018-05-09 20:44:40 +1200

    Add fulltextsearch and extension fix
    As the module requires full text search, I have enabled it here.
    
    Additionally, FulltextSearchable adds an extension called
    ContentControllerSearchExtension which supercedes the CustomSearch and
    stops it working at all. This is why it is necessary to remove the
    ContentControllerSearchExtension straight away after  enabling
    FulltextSearchable.

--- _config.php
+++ _config.php
@@ -1,3 +1,12 @@
 <?php
 
+use SilverStripe\CMS\Controllers\ContentController;
+use SilverStripe\ORM\Search\FulltextSearchable;
+
 define('SEARCHABLE_DATAOBJECTS_DIR', basename(dirname(__FILE__)));
+
+// Enable full text search. This is required by the module
+FulltextSearchable::enable();
+
+// Remove ContentControllerSearchExtension as it conflicts with the CustomSearch extension
+ContentController::remove_extension("SilverStripe\\CMS\\Search\\ContentControllerSearchExtension");
